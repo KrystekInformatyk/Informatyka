6f0d72c4c24d1acbdd655451767897be38db002d
KrystekInformatyk <144493310+KrystekInformatyk@users.noreply.github.com>
2023-09-11 20:30:34 +0200

    Create podstawy.py

---
+++ podstawy.py
@@ -0,0 +1,34 @@
+print('Hello, World !')
+
+
+print(10+10)
+print(10-10)
+print(10/10)
+print(10*10)
+print(10**10)
+print(10%2)
+print(11%2)
+
+
+print('-'*100)
+x = 10 
+y = 11
+print(x+y)
+
+
+a = input('Podaj liczbe a')
+b = float(input('Podaj liczbe b'))
+print('-'*100)
+print(a, 'i', b)
+print(a == b)
+# Czemu tu jest błąd jak go poprawić? 
+# print(a <= b) 
+print(a != b)
+
+print(5%5)
+print(10%5)
+print(15%5)
+print(20%5)
+print(25%5)
+print(30%5)
+print(35%5)
