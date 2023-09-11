1c4da9b56c16f2f59ee3c23f0601a4450a63f850
KrystekInformatyk <144493310+KrystekInformatyk@users.noreply.github.com>
2023-09-11 18:17:11 +0200

    Create trojkat.py

---
+++ trojkat.py
@@ -0,0 +1,12 @@
+a = float(input('podaj 1 bok '))
+b = float(input('podaj 2 bok '))
+c = float(input('podaj 3 bok '))
+
+
+if a + b > c:
+    if a + c > b:
+    	if b + c > a:
+            print('DA! się zbudować trójkąta')
+    else:
+        	print('NIE! da się zbudować trójkąta')
+
